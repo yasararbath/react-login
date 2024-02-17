d1c895e6694e0636662b2227571c938597323ed8
YasarSikkanthar <ysikkanthar@waymore.io>
2024-02-17 16:40:48 +0530

    dynamic forms changes

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,12 +1,24 @@
 import { DyanmicForms } from "../common/complex.forms/Dynamic.forms";
+import { DyanmicForms as SimpleForm } from "../common/forms/Dynamic.forms";
 import { fields } from "../common/complex.forms/data";
-
+import { fields as simpleFormFields } from "../common/forms/data";
+import './Dashboard.css'
+import { useState } from "react";
 
 export const Dashboard=()=> {
+  const [typeForm, SetTypeForm]= useState('');
     return (
-      <div className="App">
-        <h1>Dynamic form</h1>
-        <DyanmicForms fields={fields} />
+      <div className="dashboard">
+       <div className={typeForm ? 'header-tag' : ''}>
+       <h1>{typeForm ? typeForm.toUpperCase() : 'DYNAMIC'} FORM</h1>
+        { typeForm && <div><button className="button" onClick={e=>{SetTypeForm('')}}>Back</button></div>}
+       </div>
+        {!typeForm && <div  className="button-container">
+          <span className="button" onClick={e=>{SetTypeForm('simple')}} >Simple</span>
+          <span className="button" onClick={e=>{SetTypeForm('complex')}}>Complex</span>
+        </div>}
+        { typeForm === 'simple' && <SimpleForm fields={simpleFormFields} />}
+        { typeForm === 'complex' && <DyanmicForms fields={fields} />}
       </div>
     );
   }
